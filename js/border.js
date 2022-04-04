eb0209c2b3f49222738ffb3b2f10c4ad9a213ad4
XiaoDev <59505276+XiaoDev114@users.noreply.github.com>
2022-04-04 21:25:31 +0800

    Add files via upload

--- js/border.js
+++ js/border.js
@@ -1,3 +1,16 @@
 function addBorder(Person_name){
 	document.getElementById(Person_name).border="1";
 }
+function videohide() {
+    document.getElementById("start").style.display="none";//none or block
+}
+
+setTimeout(videohide,6000) //等待6kms执行videohide函数
+function texthide(){
+	document.getElementById("relation").style.display="none";
+}
+
+function textblock(){
+	document.getElementById("relation").style.display="block";
+}
+setTimeout(textblock,6000)
