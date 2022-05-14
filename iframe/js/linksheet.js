c38e4fd071501e447649cad08ceae330126b6960
XiaoDev <59505276+XiaoDev114@users.noreply.github.com>
2022-05-14 19:15:13 +0800

    Add files via upload

---
+++ iframe/js/linksheet.js
@@ -0,0 +1,9 @@
+function change(a){
+	var css=document.getElementByIdx_x(“cssid”);
+	if (a==1)
+	css.setAttribute(“href”,”css1.css”);
+	if (a==2)
+	css.setAttribute(“href”,”css2.css”);
+	if (a==3)
+	css.setAttribute(“href”,”css3.css”);
+}
